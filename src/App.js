fe2dc8f886a02deec02b1d1bb22864987257f050
floss-order <flossOrder@gmail.com>
2022-07-01 11:41:09 +0300

    routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import Search from "./pages/Search";
+import Sidebar from "./components/Sidebar";
+import { Box, Flex } from "@chakra-ui/react";
+import { Routes, Route, } from "react-router-dom";
+import Home from './pages/Home';
+import Transaction from "./pages/Transaction";
+import Carbon from "./pages/Carbon";
+
 function App() {
   return (
-    <div>
-      <p>something</p>
-    </div>
+    <Flex bg='gray.100'>
+      <Sidebar />
+      <Box w='100vw' h='100vh' overflow='hidden'>
+        <Box p={8} overflow='scroll' h='100vh'>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/search" element={<Search />} />
+              <Route path="/transactions/:transaction" element={<Transaction />} />
+              <Route path="/carbon" element={<Carbon />} />
+            </Routes>
+          </Box>
+      </Box>
+    </Flex>
   );
 }
 
