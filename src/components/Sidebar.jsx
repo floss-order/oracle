d33df28d34830a166c0cbbe92b9b4c92be5de0cb
floss-order <flossOrder@gmail.com>
2022-07-01 14:50:53 +0300

    implemented sidebar component

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import { Flex, IconButton } from '@chakra-ui/react';
+import { FiMenu, FiHome, FiMinus, FiBox } from 'react-icons/fi';
+import NavItem from '../components/NavItem';
+
+function Sidebar() {
+    const [navSize, changeNavSize] = useState("large");
+    return (
+        <Flex
+            // pos="sticky"
+            h="100vh"
+            boxShadow="0 4px 12px 0 rgba(0, 0, 0, 0.05)"
+            w={navSize == "small" ? "75px" : "220px"}
+            flexDir="column"
+            justifyContent="space-between"
+            bg='white'
+        >
+            <Flex
+                p="5%"
+                flexDir="column"
+                // w="100%"
+                alignItems={navSize == "small" ? "center" : "flex-start"}
+                as="nav"
+            >
+                <IconButton
+                    background="none"
+                    mt={5}
+                    _hover={{ background: 'none' }}
+                    icon={<FiMenu />}
+                    onClick={() => {
+                        if (navSize == "small")
+                            changeNavSize("large")
+                        else
+                            changeNavSize("small")
+                    }}
+                />
+                <NavItem
+                    navSize={navSize}
+                    icon={FiHome}
+                    title="Главная"
+                    active
+                    to="/"
+                />
+                <NavItem
+                    navSize={navSize}
+                    icon={FiBox}
+                    title="Карбоновый след"
+                    to="/carbon"
+                />
+                <NavItem
+                    navSize={navSize}
+                    icon={FiMinus}
+                    title="Прочее"
+                    to="/smth"
+                />
+            </Flex>
+        </Flex>
+    );
+};
+
+export default Sidebar;
