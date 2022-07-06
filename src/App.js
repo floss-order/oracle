5a4290bcf2f352f75179fdd8673fc34097c6ddfe
floss-order <flossOrder@gmail.com>
2022-07-06 11:03:23 +0300

    installed react-query

--- src/App.js
+++ src/App.js
@@ -4,21 +4,34 @@ import { Box, Flex } from "@chakra-ui/react";
 import { Routes, Route, } from "react-router-dom";
 import Home from './pages/Home';
 import Transaction from "./pages/Transaction";
+import Project from "./pages/Project";
+import { useQuery } from 'react-query';
 import Carbon from "./pages/Carbon";
 
 function App() {
+  const { isLoading, error, data } = useQuery('projectsList', () =>
+    fetch('http://localhost:3004/projects').then(res =>
+      res.json()
+    )
+  );
+
+  if (isLoading) return 'Loading...';
+  if (error) return 'An error has occurred: ' + error.message;
+
   return (
     <Flex bg='gray.100'>
-      <Sidebar />
+      <Sidebar projects={data} />
       <Box w='100vw' h='100vh' overflow='hidden'>
         <Box p={8} overflow='scroll' h='100vh'>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/search" element={<Search />} />
-              <Route path="/transactions/:transaction" element={<Transaction />} />
-              <Route path="/carbon" element={<Carbon />} />
-            </Routes>
-          </Box>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/transactions/:transaction" element={<Transaction />} />
+            {data.map((project, index) => (
+              <Route key={index} path={`/${project.slug}`} element={<Carbon apiURL={project.apiURL} />} />
+            ))}
+          </Routes>
+        </Box>
       </Box>
     </Flex>
   );
--- src/index.js
+++ src/index.js
@@ -4,14 +4,19 @@ import './index.css';
 import App from './App';
 import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter as Router } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const queryClient = new QueryClient();
+
 root.render(
   <React.StrictMode>
     <Router>
-      <ChakraProvider>
-        <App />
-      </ChakraProvider>
+      <QueryClientProvider client={queryClient}>
+        <ChakraProvider>
+          <App />
+        </ChakraProvider>
+      </QueryClientProvider>
     </Router>
   </React.StrictMode>
 );
