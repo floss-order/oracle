43e5db629172cad779d51d75a20c50ca25f25ab0
floss-order <flossOrder@gmail.com>
2022-07-15 16:50:09 +0300

    adjusted height

--- src/components/Toolbox.jsx
+++ src/components/Toolbox.jsx
@@ -3,28 +3,23 @@ import { Box, Button, Heading, Stack, Text } from "@chakra-ui/react";
 import ToolboxItem from "./ToolboxItem";
 import LoadCircle from "./LoadCircle";
 import InfoCard from './InfoCard';
+import SearchBox from "./SearchBox";
+import Chart from "./Chart";
 
 function Toolbox() {
     return (
-        <Box h="100vh" bg="white" p={6} overflowY="scroll" overflowX="hidden">
+        <Box maxH="3xl" w="xl" bg="white" p={6} overflowY="scroll" overflowX="hidden">
             <Heading size="md">Компоненты</Heading>
             <Stack spacing={4}>
                 <ToolboxItem title="Графики">
-                    <Button>Something 1</Button>
-                    <Button>Something 2</Button>
-                    <Button>Something 2</Button>
-                    <Button>Something 2</Button>
-                    <Button>Something 2</Button>
-                    <Button>Something 2</Button>
-                    <Button>Something 2</Button>
-                    <Button>Something 2</Button>
-                    <Button>Something 2</Button>
-                    <Button>Something 2</Button>
-                    <Button>Something 3</Button>
+                    <Chart />
                 </ToolboxItem>
                 <ToolboxItem title="Показатели">
                     <LoadCircle title="Нагрузка" load={40} minW="100%" />
                     <InfoCard title="Карточка" value={4000} minW="100%" />
+                </ToolboxItem>
+                <ToolboxItem title="Прочее">
+                    <SearchBox />
                 </ToolboxItem>
             </Stack>
         </Box>
