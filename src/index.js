44a5128128f9662aa8af41f70224e5ec51d63e08
floss-order <flossOrder@gmail.com>
2022-07-21 11:35:13 +0300

    disabled the strict mode

--- src/index.js
+++ src/index.js
@@ -1,22 +1,38 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter as Router } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from 'react-query';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// const queryClient = new QueryClient();
+
+// root.render(
+//   <React.StrictMode>
+//     <Router>
+//       <QueryClientProvider client={queryClient}>
+//         <ChakraProvider>
+//           <App />
+//         </ChakraProvider>
+//       </QueryClientProvider>
+//     </Router>
+//   </React.StrictMode>
+// );
+
 const queryClient = new QueryClient();
 
-root.render(
-  <React.StrictMode>
+ReactDOM.render(
+  // <React.StrictMode>
     <Router>
       <QueryClientProvider client={queryClient}>
         <ChakraProvider>
           <App />
         </ChakraProvider>
       </QueryClientProvider>
-    </Router>
-  </React.StrictMode>
+    </Router>,
+  // </React.StrictMode>,
+  document.getElementById('root')
 );
