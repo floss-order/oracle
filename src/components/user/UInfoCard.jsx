3619416dbb6d90038ee2c41ab2d6cfb107b3d954
floss-order <flossOrder@gmail.com>
2022-07-21 13:56:04 +0300

    info card droppable component with settings

--- src/components/user/UInfoCard.jsx
+++ src/components/user/UInfoCard.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import InfoCard from '../InfoCard';
 import { useNode } from '@craftjs/core';
+import { Input, FormControl, FormLabel, FormHelperText, Stack } from '@chakra-ui/react';
 
 function UInfoCard({ title, value, ...props }) {
     const { connectors: { connect, drag } } = useNode();
@@ -12,6 +13,47 @@ function UInfoCard({ title, value, ...props }) {
             {...props}
         />
     );
+};
+
+function UInfoCardSettings() {
+    const titleRef = useRef();
+    const valueRef = useRef();
+
+    const { title, value, actions: { setProp } } = useNode((node) => ({
+        title: node.data.props.title,
+        value: node.data.props.value
+    }));
+
+    return (
+        <FormControl>
+            <Stack>
+                <FormLabel>Заголовок</FormLabel>
+                <Input
+                    ref={titleRef}
+                    onChange={() => setProp((props) => props.title = titleRef.current.value)}
+                    value={title}
+                    size="lg"
+                />
+                <FormLabel>Показатель</FormLabel>
+                <Input
+                    ref={valueRef}
+                    onChange={() => setProp((props) => props.value = valueRef.current.value)}
+                    value={value}
+                    size="lg"
+                />
+            </Stack>
+        </FormControl>
+    )
+}
+
+UInfoCard.craft = {
+    props: {
+        title: "заголовок",
+        value: 3000
+    },
+    related: {
+        settings: UInfoCardSettings
+    }
 };
 
 export default UInfoCard;
