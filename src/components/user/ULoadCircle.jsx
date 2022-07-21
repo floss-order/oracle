d93a4489e4838f69da27531a8a13ee1dcbcf6956
floss-order <flossOrder@gmail.com>
2022-07-21 11:34:08 +0300

    implemented settings for custom load circle

--- src/components/user/ULoadCircle.jsx
+++ src/components/user/ULoadCircle.jsx
@@ -1,6 +1,7 @@
 import React, { useRef } from 'react';
 import { useNode } from '@craftjs/core';
 import LoadCircle from '../LoadCircle';
+import { Input, FormControl, FormLabel, FormHelperText, Stack } from '@chakra-ui/react';
 
 function ULoadCircle({ title, load, ...props }) {
     const { connectors: { connect, drag } } = useNode();
@@ -8,5 +9,49 @@ function ULoadCircle({ title, load, ...props }) {
         <LoadCircle title={title} load={load} ref={ref => connect(drag(ref))} {...props} />
     );
 }
+
+function ULoadCircleSetting() {
+    const titleRef = useRef();
+    const loadRef = useRef();
+
+    const { title, load, actions: { setProp } } = useNode((node) => ({
+        title: node.data.props.title,
+        load: node.data.props.load
+    }));
+
+    return (
+        <>
+            <FormControl>
+                <Stack>
+                    <FormLabel>Заголовок</FormLabel>
+                    <Input
+                        ref={titleRef}
+                        onChange={() => setProp((props) => props.title = titleRef.current.value)}
+                        value={title}
+                        size="lg"
+                    />
+                    <FormLabel>Значение нагрузки</FormLabel>
+                    <Input
+                        ref={loadRef}
+                        onChange={() => setProp((props) => props.load = loadRef.current.value)}
+                        value={load}
+                        size="lg"
+                    />
+                </Stack>
+            </FormControl>
+        </>
+    )
+};
+
+ULoadCircle.craft = {
+    props: {
+        title: "нагрузка",
+        load: 40
+    },
+    related: {
+        settings: ULoadCircleSetting
+    }
+};
+
 
 export default ULoadCircle
