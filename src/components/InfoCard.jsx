501c3b9d03eecbcc46b0ccc6caa6a24ed6f26d7e
floss-order <flossOrder@gmail.com>
2022-07-21 12:20:01 +0300

    ref forwarding for infocard

--- src/components/InfoCard.jsx
+++ src/components/InfoCard.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Box, Center, Heading, Text } from '@chakra-ui/react';
 
-function InfoCard({ title, value, minW }) {
+const InfoCard = React.forwardRef(({ title, value, minW }, ref) => {
     return (
-        <Box bg='white' p={4} borderWidth='1px' borderRadius='lg' minW={minW}>
+        <Box bg='white' p={4} borderWidth='1px' borderRadius='lg' minW={minW} ref={ref}>
             <Heading color="gray.400" as='h3' size='md'>{title}</Heading>
             <Center>
                 <Text noOfLines={3} mt={4} fontSize='5xl'>{value}</Text>
             </Center>
         </Box>
     );
-};
+});
 
 export default InfoCard;
