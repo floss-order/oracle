6c8ccd957de54937c8f23f2d836d27dac6952160
floss-order <flossOrder@gmail.com>
2022-07-25 16:32:45 +0300

    added few lines

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,54 +1,34 @@
 import React, { useState } from 'react';
-import { useQuery } from 'react-query';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-// import data from "./data.json"
-import paginate from '../utils/paginate';
 import { Button } from '@chakra-ui/react';
 import epochToDate from '../utils/epochToDate';
+import randomColor from '../utils/randomColor';
 
-const Chart = React.forwardRef((props, ref) => {
-    const [currentPage, setCurrentPage] = useState(1);
-    const [dataArray, setDataArray] = useState([]);
-    const { isLoading, data, error } = useQuery('devices', () => fetch('http://192.168.2.201:8001/api/v1/data').then(res =>
-        res.json()
-    ));
-
-    if (error) {
-        return (
-            <p>Error</p>
-        )
-    };
-
-    if (isLoading) {
-        return (
-            <p>Loading...</p>
-        )
-    };
-
-    // console.log(data);
-
-    function handleClick() {
-        setCurrentPage(currentPage + 1);
-        console.log(paginate(data['Picarro Flux Processor'], 1000, currentPage));
-    };
-
+const Chart = React.forwardRef(({ data }, ref) => {
     return (
         <>
             <Button colorScheme="blue">-</Button>
-            <Button colorScheme="facebook" onClick={handleClick}>+ {currentPage}</Button>
-            <div style={{ width: '100%', height: 500 }} ref={ref} {...props}>
+            <Button colorScheme="facebook">+</Button>
+            <div style={{ width: '100%', height: 500 }} ref={ref}>
                 <ResponsiveContainer>
                     <LineChart
-                        data={data['Picarro Flux Processor']}
+                        data={data}
                         margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
                     >
                         <XAxis dataKey="epoch_time" tickFormatter={(epoch) => epochToDate(epoch)} fontWeight="600" minTickGap={40} />
-                        <Tooltip labelFormatter={(epoch) => epochToDate(epoch, true)} labelStyle={{fontWeight: 'bold'}} />
+                        <Tooltip labelFormatter={(epoch) => epochToDate(epoch, true)} labelStyle={{ fontWeight: 'bold' }} />
                         <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-                        <Line type="monotone" dataKey="nh3" stroke="#ff7300" yAxisId={0} />
+                        <Line type="monotone" dataKey="ch4" stroke="#ff7300" yAxisId={0} />
                         <Line type="monotone" dataKey="co2" stroke="#387908" yAxisId={1} />
-                        <Line type="monotone" dataKey="ch4" stroke="#767508" yAxisId={2} />
-                        <Legend verticalAlign="bottom" height={36} />
+                        <Line type="monotone" dataKey="h2o" stroke="#767508" yAxisId={2} />
+                        <Line type="monotone" dataKey="n20" stroke="#226408" yAxisId={3} />
+                        <Line type="monotone" dataKey="nh3" stroke="#923469" yAxisId={4} />
+                        {/* {
+                            Object.keys(data['Picarro Flux Processor'][0]).map((key, index) => (
+                                <Line type="monotone" dataKey={key} stroke={randomColor()} yAxisId={index} key={index} />
+                            ))
+                        } */}
+                        <Legend verticalAlign="bottom" />
                     </LineChart>
                 </ResponsiveContainer>
             </div>
