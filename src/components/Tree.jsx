166e2f9e78bc25a39cdb22d5ab7243c9c690126d
floss-order <flossOrder@gmail.com>
2022-08-02 01:58:54 +0300

    checkbox tree

---
+++ src/components/Tree.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Box, CheckboxGroup, Checkbox, Stack } from "@chakra-ui/react";
+
+function convertObject(object) {
+    return Object.entries(object).map(([key, value]) => ({ [key]: value }));
+}
+
+const Tree = ({ data = [], onChange }) => {
+    return (
+        <CheckboxGroup onChange={onChange}>
+            <Stack>
+                {
+                    Object.keys(data[0]).map((key, index) => (
+                        <Checkbox value={key} key={index}>{key}</Checkbox>
+                    ))
+                }
+            </Stack>
+        </CheckboxGroup>
+    );
+};
+
+export default Tree;
