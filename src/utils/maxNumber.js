a150abca7d04149c89fa4453922af9109881ba24
floss-order <flossOrder@gmail.com>
2022-08-02 00:50:40 +0300

    rounding to hundredth

--- src/utils/maxNumber.js
+++ src/utils/maxNumber.js
@@ -1,5 +1,10 @@
 export function maxNumber(array, keys) {
     const values = array.map((object, index) => keys.map((key, index) => object[key]));
     const mergedValues = values.reduce((a, b) => a.concat(b), []);
-    return Math.max(...mergedValues);
+    const max = Math.max(...mergedValues)
+    return roundNumber(max);
+};
+
+function roundNumber(number) {
+    return Math.ceil(number/100) * 100;
 };
