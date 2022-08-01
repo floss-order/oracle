7c54e269355770d6da594f0b0da2db92b8adb910
floss-order <flossOrder@gmail.com>
2022-08-02 00:50:18 +0300

    getting only necessary object keys

--- src/utils/maxNumber.js
+++ src/utils/maxNumber.js
@@ -1,5 +1,5 @@
-export function maxNumber(array) {
-    const values = array.map((object, index) => Object.values(object));
+export function maxNumber(array, keys) {
+    const values = array.map((object, index) => keys.map((key, index) => object[key]));
     const mergedValues = values.reduce((a, b) => a.concat(b), []);
     return Math.max(...mergedValues);
 };
